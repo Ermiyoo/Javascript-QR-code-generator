e19db857d7a2d43d0d2ff9a549c51bae3b35783b
Ermiyas Getachew <61278338+Ermiyoo@users.noreply.github.com>
2022-10-01 07:10:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const textContent = document.querySelector("#qr-content");
 const btnGenerate = document.querySelector("#btnGenerate");
 const qrContainer = document.querySelector("#qr-code");
+const qrSize = document.querySelector("#size");
 
 btnGenerate.addEventListener('click', () => {
   if(!(textContent.value == "" )) {
-        fetch(`https://api.qrserver.com/v1/create-qr-code/?size=350x350&data=${textContent.value}`).then(res => {
+        fetch(`https://api.qrserver.com/v1/create-qr-code/?size=${qrSize.value}&data=${textContent.value}`).then(res => {
             const qr = `<img src=${res.url}>`;
             qrContainer.innerHTML = qr;
         });
